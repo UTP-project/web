dc29434905e49f50945e62e04b74c20d11ee16fa
ddowrs <whitezyg@gmail.com>
2020-02-26 18:53:36 +0800

    chore: add material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Button } from '@material-ui/core';
 
 import './index.css';
 
-import Home from './pages/Home';
-
-const App = () => {
-  return <h1 className="red">Hello, world</h1>;
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="red">Hello, world</h1>
+      <Button>test</Button>
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById('root'));
