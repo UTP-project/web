1c7d3c4a06c68af8374b56670dd0b006861c9c51
ddowrs <whitezyg@gmail.com>
2020-04-09 23:59:03 +0800

    feat: add date range picker

--- src/pages/Itinerary/ItineraryDate.tsx
+++ src/pages/Itinerary/ItineraryDate.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
+import {
+  StaticDateRangePicker,
+  DateRange,
+  LocalizationProvider,
+} from '@material-ui/pickers';
+import { zhCN } from 'date-fns/locale';
 
 const ItineraryDate: React.FC = () => {
-  return <div>choose date</div>;
+  const [selectedDate, handleDateChange] = useState<DateRange>([null, null]);
+
+  return (
+    <LocalizationProvider dateAdapter={DateFnsUtils} locale={zhCN}>
+      <StaticDateRangePicker
+        displayStaticWrapperAs="mobile"
+        disablePast
+        startText="起始"
+        endText="结束"
+        value={selectedDate}
+        onChange={handleDateChange}
+      />
+    </LocalizationProvider>
+  );
 };
 
 export default ItineraryDate;
