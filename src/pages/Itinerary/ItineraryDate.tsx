684a798b094e55fe1eff9b7b45267e0d92a6815e
ddowrs <whitezyg@gmail.com>
2020-04-10 18:42:13 +0800

    feat: finish date page

--- src/pages/Itinerary/ItineraryDate.tsx
+++ src/pages/Itinerary/ItineraryDate.tsx
@@ -1,26 +1,87 @@
 import React, { useState } from 'react';
-import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
+import IfiniteCalendar, { Calendar, withRange } from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
+import zhCN from 'date-fns/locale/zh_cn';
 import {
-  StaticDateRangePicker,
-  DateRange,
-  LocalizationProvider,
-} from '@material-ui/pickers';
-import { zhCN } from 'date-fns/locale';
+  Button,
+  makeStyles,
+  createStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import {} from 'react-router-dom';
+
+import '../index.css';
+
+const CalendarWithRange = withRange(Calendar);
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    appbar: {
+      background: 'rgb(127, 95, 251)',
+      top: 'auto',
+      bottom: 0,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
 const ItineraryDate: React.FC = () => {
-  const [selectedDate, handleDateChange] = useState<DateRange>([null, null]);
+  const classes = useStyles();
+
+  const [start] = useState(new Date());
+  const [end] = useState(new Date());
 
   return (
-    <LocalizationProvider dateAdapter={DateFnsUtils} locale={zhCN}>
-      <StaticDateRangePicker
-        displayStaticWrapperAs="mobile"
-        disablePast
-        startText="起始"
-        endText="结束"
-        value={selectedDate}
-        onChange={handleDateChange}
+    <>
+      <AppBar className={classes.appbar} position="fixed">
+        <Toolbar>
+          <Button color="inherit" href="/itinerary">
+            返回
+          </Button>
+          <Typography className={classes.title} />
+          <Button color="inherit" href="/itinerary/info">
+            下一步
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <IfiniteCalendar
+        Component={CalendarWithRange}
+        theme={{
+          selectionColor: 'rgb(146, 118, 255)',
+          textColor: {
+            default: '#333',
+            active: '#FFF',
+          },
+          weekdayColor: 'rgb(146, 118, 255)',
+          headerColor: 'rgb(127, 95, 251)',
+          floatingNav: {
+            background: 'rgba(81, 67, 138, 0.96)',
+            color: '#FFF',
+            chevron: '#FFA726',
+          },
+        }}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        selected={{
+          start,
+          end,
+        }}
+        min={new Date()}
+        minDate={new Date()}
+        locale={{
+          locale: zhCN,
+          weekdays: ['日', '一', '二', '三', '四', '五', '六'],
+          todayLabel: {
+            long: '今天',
+          },
+          headerFormat: 'MMM Do',
+        }}
       />
-    </LocalizationProvider>
+    </>
   );
 };
 
