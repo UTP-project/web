a19d867b8ed1f4be0ec508cf2861657da9fd8e03
ddowrs <whitezyg@gmail.com>
2020-04-10 20:39:32 +0800

    feat: finish info select page

--- src/pages/Itinerary/ItineraryInfo.tsx
+++ src/pages/Itinerary/ItineraryInfo.tsx
@@ -1,7 +1,165 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import '../index.css';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  makeStyles,
+  createStyles,
+  Theme,
+  Typography,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Grid,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '100%',
+    },
+    appbar: {
+      background: 'rgb(127, 95, 251)',
+      top: 'auto',
+      bottom: 0,
+    },
+    mid: {
+      flexGrow: 1,
+    },
+    formContainer: {
+      marginTop: 60,
+      height: window.innerHeight * 0.8 - 60,
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 200,
+    },
+  })
+);
 
 const ItineraryInfo: React.FC = () => {
-  return <div>select info</div>;
+  const classes = useStyles();
+
+  const [peopleNum, setPeopleNum] = useState('0');
+  const [crowdType, setCrowdType] = useState('0');
+  const [travelMode, setTravelMode] = useState('0');
+  const [dayTime, setDayTime] = useState('0');
+
+  const handlePeopleNumChange = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ): void => {
+    setPeopleNum(event.target.value as string);
+  };
+
+  const handleCrowdType = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ): void => {
+    setCrowdType(event.target.value as string);
+  };
+
+  const handleTravelMode = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ): void => {
+    setTravelMode(event.target.value as string);
+  };
+
+  const handleDayTime = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ): void => {
+    setDayTime(event.target.value as string);
+  };
+
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar className={classes.appbar} position="fixed">
+          <Toolbar>
+            <Button color="inherit" href="/itinerary/new/date">
+              上一步
+            </Button>
+            <Typography className={classes.mid} />
+            <Button color="inherit" href="/itinerary/new/city">
+              下一步
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Grid
+          className={classes.formContainer}
+          container
+          direction="column"
+          justify="space-around"
+          alignItems="center"
+        >
+          <FormControl className={classes.formControl}>
+            <InputLabel id="people-number-label">人数</InputLabel>
+            <Select
+              labelId="people-number-label"
+              id="people-number"
+              value={peopleNum}
+              onChange={handlePeopleNumChange}
+            >
+              <MenuItem value="0">不限</MenuItem>
+              <MenuItem value="1">1人</MenuItem>
+              <MenuItem value="2">2人</MenuItem>
+              <MenuItem value="3">3~5人</MenuItem>
+              <MenuItem value="4">6~10人</MenuItem>
+              <MenuItem value="5">10人以上</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="crowd-type-label">人群</InputLabel>
+            <Select
+              labelId="crowd-type-label"
+              id="crowd-type"
+              value={crowdType}
+              onChange={handleCrowdType}
+            >
+              <MenuItem value="0">不限</MenuItem>
+              <MenuItem value="1">学生</MenuItem>
+              <MenuItem value="2">同事</MenuItem>
+              <MenuItem value="3">亲子</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="travel-mode-label">主要出行方式</InputLabel>
+            <Select
+              labelId="travel-mode-label"
+              id="travel-mode"
+              value={travelMode}
+              onChange={handleTravelMode}
+            >
+              <MenuItem value="0">不限</MenuItem>
+              <MenuItem value="1">驾车</MenuItem>
+              <MenuItem value="2">公交</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="day-time-label">每日游玩时间</InputLabel>
+            <Select
+              labelId="day-time-label"
+              id="day-time"
+              value={dayTime}
+              onChange={handleDayTime}
+            >
+              <MenuItem value="0">3小时</MenuItem>
+              <MenuItem value="1">4小时</MenuItem>
+              <MenuItem value="2">5小时</MenuItem>
+              <MenuItem value="3">6小时</MenuItem>
+              <MenuItem value="4">7小时</MenuItem>
+              <MenuItem value="5">8小时</MenuItem>
+              <MenuItem value="6">9小时</MenuItem>
+              <MenuItem value="7">10小时</MenuItem>
+              <MenuItem value="8">11小时</MenuItem>
+              <MenuItem value="9">12小时</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </div>
+    </>
+  );
 };
 
 export default ItineraryInfo;
